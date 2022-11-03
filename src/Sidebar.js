006616df59197a166b4e96873794abfb71f7e47c
Ethan Salonga <ethansalonga28@gmail.com>
2022-11-03 13:48:25 -0400

    Build and style SidebarOptions

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,20 +2,28 @@ import React from 'react'
 import "./Sidebar.css"
 import TwitterIcon from '@mui/icons-material/Twitter'
 import SidebarOption from './SidebarOption'
+import HomeIcon from '@mui/icons-material/Home'
+import TagIcon from '@mui/icons-material/Tag';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone'
+import MailOutlineIcon from '@mui/icons-material/MailOutline'
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder'
+import ListAltIcon from '@mui/icons-material/ListAlt'
+import PermIdentityIcon from '@mui/icons-material/PermIdentity'
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz'
 
 function Sidebar() {
   return (
     <div className="sidebar">
       <TwitterIcon />
 
-      <SidebarOption text="Home" />
-      <SidebarOption text="Explore" />
-      <SidebarOption text="Notifications" />
-      {/* SidebarOption */}
-      {/* SidebarOption */}
-      {/* SidebarOption */}
-      {/* SidebarOption */}
-      {/* SidebarOption */}
+      <SidebarOption active Icon={HomeIcon} text="Home" />
+      <SidebarOption Icon={TagIcon} text="Explore" />
+      <SidebarOption Icon={NotificationsNoneIcon} text="Notifications" />
+      <SidebarOption Icon={MailOutlineIcon} text="Messages" />
+      <SidebarOption Icon={BookmarkBorderIcon} text="Bookmarks" />
+      <SidebarOption Icon={ListAltIcon} text="Lists" />
+      <SidebarOption Icon={PermIdentityIcon} text="Profile" />
+      <SidebarOption Icon={MoreHorizIcon} text="More" />
 
       {/* Button -> Tweet */}
     </div>
