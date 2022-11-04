00aaeab9743bf31890e1249d499df4894c6120f2
Ethan Salonga <ethansalonga28@gmail.com>
2022-11-04 12:52:37 -0400

    Style TweetBox button

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -10,9 +10,13 @@ function TweetBox() {
         <div className="tweetBox__input">
           <Avatar src="https://media-exp1.licdn.com/dms/image/C4D03AQH5-vBMsJPW0Q/profile-displayphoto-shrink_800_800/0/1636484250547?e=1672876800&v=beta&t=TSz9iMnl_aMF7Y28l033x0rhtVViOLXJcRKfkCuT62U" />
           <input placeholder="What's happening?" type="text" />
-          {/* <input placeholder="Enter image URL" type="text" /> */}
         </div>
-        <Button>Tweet</Button>
+        <input
+          className="tweetBox__imageInput"
+          placeholder="Optional: Enter image URL"
+          type="text"
+        />
+        <Button className="tweetBox__tweetButton">Tweet</Button>
       </form>
     </div>
   )
