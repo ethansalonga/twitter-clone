7861d0a7cac70acbd6c03a35683e289944b1e8f5
Ethan Salonga <ethansalonga28@gmail.com>
2022-11-05 14:29:31 -0400

    Setup and connect Firestore

--- src/Feed.js
+++ src/Feed.js
@@ -1,26 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Feed.css"
 import TweetBox from './TweetBox'
 import Post from './Post'
+import db from "./firebase"
 
 function Feed() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    db.collection("posts").onSnapshot(snapshot => (
+      setPosts(snapshot.docs.map(doc => doc.data()))
+    ))
+  }, [])
+
   return (
     <div className="feed">
       <div className="feed__header">
         <h2>Home</h2>
       </div>
       <TweetBox />
-      <Post
-        displayName="Ethan Salonga"
-        username="ethansalonga"
-        verified
-        text="A man chooses. A slave obeys."
-        avatar="https://media-exp1.licdn.com/dms/image/C4D03AQH5-vBMsJPW0Q/profile-displayphoto-shrink_800_800/0/1636484250547?e=1672876800&v=beta&t=TSz9iMnl_aMF7Y28l033x0rhtVViOLXJcRKfkCuT62U"
-        image="https://media.giphy.com/media/O9VvyvGXtEnp4pcN1d/giphy.gif"
-      />
-      <Post />
-      <Post />
-      <Post />
+
+      {posts.map(post => (
+        <Post
+          displayName={post.displayName}
+          username={post.username}
+          verified={post.verified}
+          text={post.text}
+          avatar={post.avatar}
+          image={post.image}
+        />
+      ))}
     </div>
   )
 }
